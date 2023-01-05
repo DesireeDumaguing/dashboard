f28a85e530f8122cf1da2e24c688a0f945115376
b-akari <108397871+b-akari@users.noreply.github.com>
2023-01-05 23:24:05 +0800

    Update account.php

--- account.php
+++ account.php
@@ -1,13 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php include 'libraries.php' ?>
+    <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
 <body>
     <?php include 'sidenavbar.php' ?>
+
+    <section id="content">
+        <main>
+            <div class="account">
+                <div class="pfp">
+                    <img src="img/pic.jpg" style="width: 350px; border-radius: 20px;">
+                </div>
+                
+                <div class="information">
+                    <h1>My Profile</h1> <br>
+                    <h2>John Doe</h2>
+                    <h3>20 yrs old</h3>
+                    <h3>johndoe@gmail.com</h3>
+                    
+                </div>
+            </div>
+        </main>
+    </section>
+
+
+    <script src="script.js"></script>
 </body>
 </html>
